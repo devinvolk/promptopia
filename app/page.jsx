bb3c0cfc6576341a225d74ada94351bbdda99c6e
devinvolk@comcast.net <devinvolk@Devins-MacBook-Air.local>
2023-09-05 14:38:23 -0700

    home page and next-auth initial setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+import Feed from '@components/Feed';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI powered prompts</span>
+      </h1>
+      <p className="desc text-center">Promptopia is an open-source AI prompting tool
+       to streamline and improve your interaction with ChatGPT
+      </p>
+
+      <Feed />
+    </section>
   )
 }
 
